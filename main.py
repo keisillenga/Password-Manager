70391eb3a04fae6af3485eceba2d6e5d6b8af2dc
Kejsi Llenga <keisillenga@gmail.com>
2026-07-17 09:03:30 +0200

    main.py

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ import string
 import os
 
 FILE_NAME = "password-manager"
+def load_password():
+  if not os.path.exists(FILE_NAME):
+    return{}
+
+   with open(FILE_NAME, "r") as file:
+     try:
+       return json.load(file)
+     except json.JSONDcodeError:
+       return{}
